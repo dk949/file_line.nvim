ef503991782c8191eb31a0b7e0b6974babd205f7
dk949 <56653556+dk949@users.noreply.github.com>
2025-04-11 01:20:50 +0100

    init

---
+++ README.md
@@ -0,0 +1,66 @@
+# file_line.nvim
+
+
+Support `file:line:column` syntax when opening files in neovim.
+
+This is a rewrite of the this
+[file-line](https://github.com/lervag/file-line/tree/master)[^1] plugin made to
+be more flexible and easier to integrate into a modern Neovim config.
+
+> [!TIP]
+> The following line/column formats also work:
+> file(line)
+> file(line:column)
+> file:line:column:
+> file:line
+
+## Usage
+
+
+### `register`
+
+When opening new files, jump to the line and column if present at the end of the
+file name.
+
+```lua
+require "file_line".register()
+```
+
+### `filenameLineCol`
+
+
+Given a file name possibly with line and column on the end, returns them as 3
+separate values. `line` and `col` could be `nil` if they were not present in the
+file name, they will be numbers otherwise.
+
+```lua
+local name, line, col =  require "file_line".filenameLineCol(file_name)
+```
+
+### `openFileOnLine`
+
+Open a file and jump to the line and column.
+
+Last argument is optional, if supplied it is either the window id or a string
+that can be interpreted by `winnr()`. This will be the window the file is opened
+in. Current window if `nil`.
+
+Returns `true` if the file was opened.
+
+```lua
+require "file_line".openFileOnLine(name, line, col, winnr)
+```
+
+### `onOpen`
+
+Callback invoked when a file is opened with `openFileOnLine`. `line` and `col`
+are numbers, if column was not specified when opening the file is it is set to
+0.
+
+```lua
+require "file_line".onOpen(function (name, line, col, bufnr)  end)
+```
+
+
+
+[^1]: Which is itself a fork of [bogado's file-line](https://github.com/bogado/file-line).
---
+++ lua/file_line/init.lua
@@ -0,0 +1,88 @@
+local M = {}
+
+---@type (fun(name:string, line:number, col:number, bufnr:number):nil)?
+local _on_open = nil
+
+---Given a file name with line and column numbers, split it into those components
+---@param name string
+---@return string
+---@return string?
+---@return string?
+function M.filenameLineCol(name)
+    -- If the input contains a parenthesis, try the (line) formats.
+    if name:find("%(") then
+        local file, line, col = name:match("^(.-)%((%d+):(%d+)%)$")
+        if file then return file, line, col end
+
+        file, line = name:match("^(.-)%((%d+)%)$")
+        if file then return file, line, nil end
+        -- Otherwise, if it contains a colon, try the colon-separated formats.
+    elseif name:find(":") then
+        local file, line, col = name:match("^(.-):(%d+):(%d+)$")
+        if file then return file, line, col end
+
+        file, line = name:match("^(.-):(%d+):$")
+        if file then return file, line, nil end
+
+        file, line = name:match("^(.-):(%d+)$")
+        if file then return file, line, nil end
+
+        return name:sub(1, name:find(":") - 1), nil, nil
+    end
+
+    -- No parenthesis or colon found; return the input as the filename.
+    return name, nil, nil
+end
+
+---Given a file name, line, column and optional buffer, open the file at that location
+---@param name string
+---@param line string?
+---@param col string?
+--- Either nil (current window), window id, or a string accepted by winnr()
+---@param winnr (string|integer)?
+function M.openFileOnLine(name, line, col, winnr)
+    name = vim.fn.fnameescape(name)
+    if line == nil and col == nil then return false end
+    local real_line = tonumber(line) or 0
+    local real_col = tonumber(col) or 0
+    if not vim.fn.filereadable(name) then return end
+
+    if winnr ~= nil then
+        if type(winnr) == "string" then winnr = vim.fn.winnr(winnr) end
+        vim.api.nvim_set_current_win(winnr)
+    end
+
+    vim.cmd("edit " .. name)
+    local new_bufnr = vim.fn.bufnr()
+    vim.cmd(tostring(real_line))
+    vim.cmd("normal!" .. real_col .. "|")
+    vim.cmd([[normal! zv]])
+    vim.cmd([[normal! zz]])
+    -- vim.cmd([[filetype detect]])
+    if _on_open ~= nil then _on_open(name, real_line, real_col, new_bufnr) end
+    return true
+end
+
+---Callback invoked when opening a file with openFileOnLine
+---@param cb fun(name:string, line:number, col:number, bufnr:number):nil
+function M.onOpen(cb)
+    _on_open = cb
+end
+
+function M.register()
+    local group = vim.api.nvim_create_augroup("file_line", {})
+    vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+        group = group,
+        pattern = "*",
+        nested = true,
+        callback = function(ev)
+            local old_alt = vim.fn.bufnr('#')
+            if M.openFileOnLine(M.filenameLineCol(ev.file)) then
+                vim.cmd("bwipeout " .. ev.buf)
+                if old_alt >= 0 then vim.fn.setreg('#', old_alt) end
+            end
+        end
+    })
+end
+
+return M
